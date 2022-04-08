16ef0097e178f70c46c5926e9c7a822d51340e0b
Ergin336 <61010417+Ergin336@users.noreply.github.com>
2022-04-08 02:18:43 +0200

    Add files via upload

---
+++ SX126X_LoRa_HAT_Code/raspberrypi/python/main.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+#
+#    this is an UART-LoRa device and thers is an firmware on Module
+#    users can transfer or receive the data directly by UART and dont
+#    need to set parameters like coderate,spread factor,etc.
+#    |============================================ |
+#    |   It does not suport LoRaWAN protocol !!!   |
+#    | ============================================|
+#   
+#    This script is mainly for Raspberry Pi 3B+, 4B, and Zero series
+#    Since PC/Laptop does not have GPIO to control HAT, it should be configured by
+#    GUI and while setting the jumpers, 
+#    Please refer to another script pc_main.py
+#
+
+import sys
+import sx126x
+import threading
+import time
+import select
+import termios
+import tty
+from threading import Timer
+
+old_settings = termios.tcgetattr(sys.stdin)
+tty.setcbreak(sys.stdin.fileno())
+
+#   serial_num
+#       PiZero, Pi3B+, and Pi4B use "/dev/ttyS0"
+#
+#    Frequency is [850 to 930], or [410 to 493] MHz
+#
+#    address is 0 to 65535
+#        under the same frequence,if set 65535,the node can receive 
+#        messages from another node of address is 0 to 65534 and similarly,
+#        the address 0 to 65534 of node can receive messages while 
+#        the another note of address is 65535 sends.
+#        otherwise two node must be same the address and frequence
+#
+#    The tramsmit power is {10, 13, 17, and 22} dBm
+#
+#    RSSI (receive signal strength indicator) is {True or False}
+#        It will print the RSSI value when it receives each message
+#
+node = sx126x.sx126x(serial_num = "/dev/ttyS0",freq=868,addr=65535,power=22,rssi=True,air_speed=2400,relay=False)
+
+try:
+    time.sleep(1)
+    while True:
+        node.receive()
+        
+        # timer,send messages automatically
+        
+except:
+    print("Fail1")
+    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
+    # print('\x1b[2A',end='\r')
+    # print(" "*100)
+    # print(" "*100)
+    # print('\x1b[2A',end='\r')
+
+termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
+# print('\x1b[2A',end='\r')
+# print(" "*100)
+# print(" "*100)
+# print('\x1b[2A',end='\r')
